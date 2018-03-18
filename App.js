25c9c1543efb0be7380b7012b22c7f040bd17283
sanab123 <sanab227@gmail.com>
2018-03-18 02:00:59 -0700

    hi

--- App.js
+++ App.js
@@ -55,43 +55,44 @@ class HomeScreen extends React.Component {
   }
 }
 class DetailsScreen extends React.Component {
-  state={list: [
-    {
-      name: 'Milk',
-      avatar_url: 'https://img1.cgtrader.com/items/717115/a69ed5d165/milk-carton-3d-model-obj-fbx-blend.png',
-      key:1
-    },
-    {
-      name: 'Cereal',
-      avatar_url: 'https://www.dollargeneral.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/c/o/compressed_19110101_hbo_strawberry_cereal.jpg',
-      key:2
-    },
-    {
-      name: 'Lays',
-      avatar_url: 'https://www.fritolay.com/images/default-source/blue-bag-image/lays-classic.png?sfvrsn=bd1e563a_2',
-      key:3
-    },
-    {
-      name: 'Milk',
-      avatar_url: 'https://img1.cgtrader.com/items/717115/a69ed5d165/milk-carton-3d-model-obj-fbx-blend.png',
-      key:4
-    },
-  ]}
   render() {
     return (
       <View>
-        <Text>details Screen</Text>
         <ScrollView>
-        {
-          this.state.list.map((item)=>{
-            return <Text>{item.name}</Text>
-          })
-        }
+          <View>
+            <Image source={require("./media/test.png")} />
+            <Button
+              title="Milk"
+              onPress={() => this.props.navigation.navigate("Mohak")}
+            >
+              <View>
+                <Text>Milk</Text>
+              </View>
+            </Button>
+          </View>
+          <View>
+            <Image source={require("./media/test.png")} />
+            <Button
+              title="Milk"
+              onPress={() => this.props.navigation.navigate("Mohak")}
+            >
+              <View>
+                <Text>Milk</Text>
+              </View>
+            </Button>
+          </View>
+          <View>
+            <Image source={require("./media/test.png")} />
+            <Button
+              title="Milk"
+              onPress={() => this.props.navigation.navigate("Mohak")}
+            >
+              <View>
+                <Text>Milk</Text>
+              </View>
+            </Button>
+          </View>
         </ScrollView>
-        <Button
-          title="Go to details"
-          onPress={() => this.props.navigation.navigate("Mohak")}
-        />
       </View>
     );
   }
